691f4a069e5c22c5dedfe5606c2b72964f0d36c2
Jack Wall <jackw1997@gmail.com>
2020-04-09 11:07:23 +0100

    Backup FYP write up progress

--- LeagueTest.py
+++ LeagueTest.py
@@ -8,5 +8,5 @@ num_of_teams = int(argv[1])
 num_of_constraints = int(argv[2])
 csv_type = argv[3]
 
-for i in range(6, num_of_teams+1):
+for i in range(14, num_of_teams+1):
     write_csv_file(i, i-2, num_of_constraints, csv_type)
